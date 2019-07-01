3de9daf14d988dd406a948ec06dbd165ffabd1fc
TianlunHu <39169925+TianlunHu@users.noreply.github.com>
2019-07-01 11:04:44 +0200

    Update SaveStream.js

--- SaveStream.js
+++ SaveStream.js
@@ -198,7 +198,7 @@ function deviceOrientationHandler (eventData) {
 
   var logo = document.getElementById("imgLogo");
   logo.style.webkitTransform = "rotate(" + tiltLR + "deg) rotate3d(1,0,0, " + (tiltFB * -1) + "deg)";
-  logo.style.MozTransform = "rotate(" + tiltLR + "deg)";
+  logo.style.MozTransform = "rotate(" + tiltLR + "deg) rotate3d(1,0,0, " + (tiltFB * -1) + "deg)";
   logo.style.transform = "rotate(" + tiltLR + "deg) rotate3d(1,0,0, " + (tiltFB * -1) + "deg)";
 }
 //----------------Motion Sensors (IMU) ---------------- //
