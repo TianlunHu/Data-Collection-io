63de36234f7249077c798e0a39198a83c7db0e41
TianlunHu <39169925+TianlunHu@users.noreply.github.com>
2019-07-05 13:31:31 +0200

    Add files via upload

--- SaveData.js
+++ SaveData.js
@@ -255,9 +255,9 @@ function StartSensor() {
         document.getElementById('moApi').innerHTML = 'Motion Sensor detected';
         window.addEventListener('deviceorientation', deviceOrientationHandler, false);
         accelerometer = new LinearAccelerationSensor({
-            frequency: 30
+            /*frequency: 30*/
         });
-        accLowPass = new LowPassFilterData(accelerometer, 0.8);
+        accLowPass = new LowPassFilterData(accelerometer, 0.95);
 
         gyroscope = new Gyroscope({
             frequency: 30
